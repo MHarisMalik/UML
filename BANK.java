0d773f32ad97512c3af08f9d7f01882a07c8ca89
MHarisMalik <47054283+MHarisMalik@users.noreply.github.com>
2020-03-24 03:08:07 +0500

    Add files via upload

---
+++ BANK.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class BANK {
+
+    /**
+     * Default constructor
+     */
+    public BANK() {
+    }
+
+    /**
+     * 
+     */
+    public void Name;
+
+    /**
+     * 
+     */
+    public void ID;
+
+    /**
+     * 
+     */
+    public void Locality;
+
+    /**
+     * 
+     */
+    public void map() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Details() {
+        // TODO implement here
+    }
+
+}
